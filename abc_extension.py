31050a554ce37197d330c66dfee3291fc9a97517
jrg909 <20345108+jrg909@users.noreply.github.com>
2019-02-28 11:59:53 -0500

    Cambios necesarios para que el algoritmo se ejecute siempre el número de iteraciones especificado.

--- abc_extension.py
+++ abc_extension.py
@@ -4,14 +4,15 @@ import copy
 
 class SolutionABCRegistrarMejores(SolutionABC):
 	
-	def __init__(self, solucionObjPadre, registrosMejores):
+	def __init__(self, solucionObjPadre, objABCRegistrarMejores):
 		super().__init__(solucionObjPadre.D, solucionObjPadre.LB, solucionObjPadre.UB)
 		self.Solution = solucionObjPadre.Solution
 		self.Fitness = solucionObjPadre.Fitness
-		self.registrosMejores = registrosMejores
+		self.objABCRegistrarMejores = objABCRegistrarMejores		
 	
 	def toString(self):
-		self.registrosMejores.agregarRegistros(self.Solution, self.Fitness) 
+		self.objABCRegistrarMejores.registros.agregarRegistros(self.Solution, self.Fitness)
+		self.objABCRegistrarMejores.aumentarContadorIteraciones()
 		#print(str(self.Solution) + " " + str(self.Fitness))
 		
 	def __deepcopy__(self, memo):
@@ -21,25 +22,26 @@ class SolutionABCRegistrarMejores(SolutionABC):
 		# Crear copia profunda del atributo Solution por ser una lista (objeto mutable)
 		solucionABCCopia.Solution = copy.deepcopy(self.Solution, memo)
 		# Crear copia
-		solucionABCRegistrarMejoresCopia = SolutionABCRegistrarMejores(solucionABCCopia, self.registrosMejores)
+		solucionABCRegistrarMejoresCopia = SolutionABCRegistrarMejores(solucionABCCopia, self.objABCRegistrarMejores)
 		return solucionABCRegistrarMejoresCopia
 
 class ABCRegistrarMejores(ArtificialBeeColonyAlgorithm):
 	
 	def __init__(self, D, NP, iteraciones, benchmark):
-		super().__init__(D, NP, self.obtenerNumeroEvaluacionesFuncion(NP, iteraciones), benchmark)
+		super().__init__(D, NP, None, benchmark)
+		self.iteracion_actual = 0
+		self.iteraciones = iteraciones
 		self.registros = RegistroMejoresABC()
-		self.Best = SolutionABCRegistrarMejores(self.Best, self.registros)
-
-	def obtenerNumeroEvaluacionesFuncion(self, poblacion, iteraciones):
-		fuentes_de_comida = int(poblacion / 2)
-		return 2 * fuentes_de_comida * iteraciones + fuentes_de_comida
 		
 	def init(self):
 		super().init()
+		self.Best = SolutionABCRegistrarMejores(self.Best, self)
 		for i in range(self.FoodNumber):
-			self.Foods[i] = SolutionABCRegistrarMejores(self.Foods[i], self.registros)
+			self.Foods[i] = SolutionABCRegistrarMejores(self.Foods[i], self)
 			self.checkForBest(self.Foods[i])
+		# Si el valor de iteraciones es 0, dar por terminado el algoritmo
+		if self.iteraciones == 0:
+			self.Done = True
 		
 	def run(self):
 		super().run()
@@ -48,19 +50,22 @@ class ABCRegistrarMejores(ArtificialBeeColonyAlgorithm):
 		
 	def tryEval(self, Solution):
 		if type(Solution) == SolutionABC:
-			indice = self.Foods[self.Foods.index(Solution)]
-			self.Foods[indice] = SolutionABCRegistrarMejores(Solution, self.registros)
-			super().tryEval(self.Foods[indice])
+			indice = self.Foods.index(Solution)
+			self.Foods[indice] = SolutionABCRegistrarMejores(Solution, self)
+			self.Foods[indice].evaluate()
 		else:
-			super().tryEval(Solution)
+			Solution.evaluate()
 		
 		if self.FEs == self.nFES:
 			self.Done = True
 	
 	def obtenerRegistros(self):
-		return self.registros.obtenerRegistros()[1:]
-		
-			
+		return self.registros.obtenerRegistros()
+
+	def aumentarContadorIteraciones(self):
+		self.iteracion_actual += 1
+		if self.iteracion_actual == self.iteraciones:
+			self.Done = True		
 
 class RegistroMejoresABC():
 	
